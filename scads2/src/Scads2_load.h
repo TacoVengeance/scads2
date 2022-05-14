c4582d9aa6ac97d307d977df55a3263ea83e886b
TacoAvenger <wingsofthehydra@gmail.com>
2022-05-14 19:31:30 -0300

    Restore to Ben-Rudiak Gold's versions

--- scads2/src/Scads2_load.h
+++
@@ -1,7 +0,0 @@
-/*
-Returns the length of the file in the first sizeof(int) bytes
-of the buffer. Kind of hacky.
-*/
-
-unsigned char *
-load_and_decrypt_tads2(const char *filename);
--- scads3/src/Scads3_load.h
+++
@@ -1,7 +0,0 @@
-/*
-Returns the length of the file in the first sizeof(int) bytes
-of the buffer. Kind of hacky.
-*/
-
-unsigned char *
-load_and_decrypt_tads3(const char *filename);
